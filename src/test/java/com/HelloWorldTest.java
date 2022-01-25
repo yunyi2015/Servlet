d73cb51677637a1f945d53281c51f14b2649e70d
Yunyi <spring_hong@126.com>
2022-01-25 11:00:31 +0800

    test

---
+++ src/test/java/com/HelloWorldTest.java
@@ -0,0 +1,25 @@
+package com;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void doGet() {
+        int i,j;
+        for (i=1;i<=10;i++);
+            j=i;
+        }
+    }
